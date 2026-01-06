d5e806fc4daf47fdc4552c561d8a3aa01cc81b25
thienanhheo6-star <thienanhheo6@gmail.com>
2026-01-06 11:26:53 +0700

    Add files via upload

---
+++ QuanLyNetContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+public class QuanLyNetContext : DbContext
+{
+    public QuanLyNetContext() : base("name=QLNET") { }
+
+    public DbSet<MAY> MAYs { get; set; }
+    public DbSet<TAIKHOAN> TAIKHOANs { get; set; }
+    public DbSet<SUDUNG> SUDUNGs { get; set; }
+    public DbSet<DICHVU> DICHVUs { get; set; }
+    public DbSet<CHITIETDICHVU> CHITIETDICHVUs { get; set; }
+}
